4cd7f8a34301b80db8de6eaa7d0ea8bd0b4196b8
Yuumataiki <84608371+Yuumataiki@users.noreply.github.com>
2021-05-29 09:10:08 +0900

    Initial Commit

---
+++ introduce/introduceApp.swift
@@ -0,0 +1,17 @@
+//
+//  introduceApp.swift
+//  introduce
+//
+//  Created by yumataiki on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct introduceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
