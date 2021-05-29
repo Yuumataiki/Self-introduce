7ae8b56dd25a0686ccc221a32d4783434affcd6c
Yuumataiki <84608371+Yuumataiki@users.noreply.github.com>
2021-05-29 09:59:33 +0900

    selfintroduce

--- introduce/ContentView.swift
+++ introduce/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("遊間大貴").font(.title).foregroundColor(Color.purple).tabItem { Text("名前") }.tag(1)
+            VStack{
+                Text("情報理工学部").font(.title).foregroundColor(Color.blue)
+                Text("2回生")
+            }.tabItem { Text("学部") }.tag(2)
+            Text("19歳").font(.title).foregroundColor(Color.red).tabItem { Text("年齢") }.tag(3)
+            Text("栃木").font(.title).foregroundColor(Color.green).tabItem { Text("出身") }.tag(4)
+            VStack{
+                Text("ゲーム").font(.title).foregroundColor(Color.gray)
+                Text("モンハン")
+            }.tabItem{ Text("趣味") }.tag(5)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
